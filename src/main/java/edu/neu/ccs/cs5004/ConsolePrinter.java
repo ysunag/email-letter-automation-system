bb070b96df09776dc975a17f9836f6b472f691b8
sunyang <sun.yang@husky.neu.edu>
2018-03-31 18:24:13 -0700

    add implementation

--- src/main/java/edu/neu/ccs/cs5004/ConsolePrinter.java
+++
@@ -1,7 +0,0 @@
-package edu.neu.ccs.cs5004;
-
-/**
- * Represents a console printer to print error message.
- */
-public class ConsolePrinter implements Printer {
-}
--- src/main/java/edu/neu/ccs/cs5004/Printer.java
+++
@@ -1,7 +0,0 @@
-package edu.neu.ccs.cs5004;
-
-/**
- * Represents a printer to print error message.
- */
-public interface Printer {
-}
