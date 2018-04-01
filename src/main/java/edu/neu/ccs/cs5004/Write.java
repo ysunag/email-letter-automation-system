8d3e150eb0bb1670e886a4a762148b01b3c9af0d
zhengzhilong <zheng.zhil@husky.neu.edu>
2018-03-31 18:26:03 -0700

    add writer

--- src/main/java/edu/neu/ccs/cs5004/Write.java
+++ src/main/java/edu/neu/ccs/cs5004/Write.java
@@ -1,5 +1,9 @@
 package edu.neu.ccs.cs5004;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.List;
 
 /**
@@ -7,7 +11,18 @@ import java.util.List;
  */
 public class Write {
 
-  public void writeIntoDir(DirPath dirPath, List<String> content) {}
+  public void writeIntoDir(String file, List<String> content) {
+    try (BufferedWriter outputFile = new BufferedWriter(new OutputStreamWriter(
+            new FileOutputStream(file), "UTF-8"))) {
+      for(String line : content) {
+        outputFile.write(line);
+        outputFile.newLine();
+      }
+    } catch (IOException ioe) {
+      System.out.println("Something went wrong! : " + ioe.getMessage());
+      ioe.printStackTrace();
+    }
+  }
 
 
 
